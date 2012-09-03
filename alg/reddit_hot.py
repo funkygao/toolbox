4b316ae3eee34350822fc750af241116e5f3b2c6
gaopeng <gaopeng@corp.kaixin001.com>
2012-09-03 10:56:57 +0800

    add confidence calucator

--- alg/reddit_hot.py
+++ alg/reddit_hot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #encoding=utf-8
 '''reddit的帖子推荐算法'''
-from math import log10
+from math import log10, sqrt
 from datetime import datetime, timedelta
 
 def epoch_seconds(date):
@@ -21,6 +21,22 @@ def hot(ups, downs, post_date):
     seconds = epoch_seconds(post_date) - 1134028003 # 1134028003是reddit公司成立的日期
     return order + sign * seconds / 45000 # 
 
+def confidence(ups, downs):
+    """The confidence sort.
+       http://www.evanmiller.org/how-not-to-sort-by-average-rating.html"""
+    n = ups + downs
+    if n == 0:
+        return 0
+
+    z = 1.281551565545 # 80% confidence
+    p = float(ups) / n
+
+    left = p + 1/(2*n)*z*z
+    right = z*sqrt(p*(1-p)/n + z*z/(4*n*n))
+    under = 1+1/n*z*z
+
+    return (left - right) / under
+
 if __name__ == '__main__':
     fixtures = [
         (156, 45, datetime(2012, 8, 23)),
@@ -31,3 +47,13 @@ if __name__ == '__main__':
     ]
     for (ups, downs, post_date) in fixtures:
         print hot(ups, downs, post_date)
+
+    print '-' * 30
+
+    fixtures = [
+        (156, 45),
+        (1560, 450),
+        (15600, 4500),
+    ]
+    for (ups, downs) in fixtures:
+        print confidence(ups, downs)
