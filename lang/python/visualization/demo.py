cb849bddca37283ca27b8f7953c1cac66a373eff
gaopeng <gaopeng@corp.kaixin001.com>
2012-09-03 18:30:25 +0800

    a simplest Matplotlib demo

--- lang/python/visualization/demo.py
+++ lang/python/visualization/demo.py
@@ -6,5 +6,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+class Demo(object):
+
+    def simplest(self):
+        x, y = [1, 2, 4, 5], [4, 9, 2, 10]
+        plt.plot(x, y)
+        plt.ylabel('some numbers')
+        plt.ylim(0, 10)
+        plt.grid(True)
+        plt.show()
+
+        #plt.plot(x, y, 'rs', x, [y**2 for y in x], 'g^')
+        plt.plot(x, y, x, [v**2 for v in y], 'g^')
+        plt.savefig('_demo.png')
+        plt.show()
+
+
 if __name__ == '__main__':
-    pass
+    demo = Demo()
+    demo.simplest()
