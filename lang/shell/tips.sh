afb25bb810b986b6299210fb14c48d238a3cbb1a
funky gao <funky.gao@gmail.com>
2012-09-07 22:46:34 +0800

    shell tips more

--- lang/shell/tips.sh
+++ lang/shell/tips.sh
@@ -1,6 +1,11 @@
 # cut more spaces as delimiter
-bzcat debug.log.2012-06-1*.bz2 | grep photo_upload_stat_statck | sed 's/\s\s*/ /g'|cut -d' ' -f 7
+bzcat debug.log.2012-06-1*.bz2 | grep photo_upload_stat_statck | sed 's/\s\s*/ /g' | cut -d' ' -f 7
 
 # replace files content on batch
 sed -i "s/组件/应用/g" `grep "组件" -rl ./`
 
+# 查看web server类型
+curl -s --head "http://www.baidu.com"
+
+# 查看cpu位数
+getconf LONG_BIT
