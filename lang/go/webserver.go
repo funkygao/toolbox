3d61473897aa47e92094d090662fe5783f82b972
funky gao <funky.gao@gmail.com>
2012-09-21 00:03:58 +0800

    concurrent max 100

--- lang/go/webserver.go
+++ lang/go/webserver.go
@@ -3,33 +3,17 @@ package main
 import (
     "fmt"
     "net/http"
-    "log"
+    "runtime"
 )
 
 func requestHandler(writer http.ResponseWriter, req *http.Request) {
-    html := `
-<!DOCTYPE html>
-<html lang="zh-cn">
-<head>
-<title>测试一下go作为web server的性能</title>
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
-<meta charset="utf-8" />
-<body>
-<div id="login-bar" class="no-visited">
-  <div class="yui3-d3f">
-      <ul class="site-login"></ul>
-  </div>
-</div>
-</body>
-</html>`
+    html := ` <!DOCTYPE html> <html lang="zh-cn"> <head> <title>测试一下go作为web server的性能</title> <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" /> <meta charset="utf-8" /> <body> <div id="login-bar" class="no-visited"> <div class="yui3-d3f"> <ul class="site-login"></ul> </div> </div> </body> </html>`
     fmt.Fprintf(writer, html)
 }
 
 func main() {
+    runtime.GOMAXPROCS(100)  // 并发协程数
     http.HandleFunc("/", requestHandler)
-    err := http.ListenAndServe(":9000", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+    http.ListenAndServe(":9000", nil)
 }
 
