ce368055643e8a05bd68a57c5e6fc3b5ff375ef8
gaopeng <gaopeng@corp.kaixin001.com>
2012-09-21 14:12:45 +0800

    released

--- lang/go/gitfiles.go
+++ lang/go/gitfiles.go
@@ -3,7 +3,6 @@
  */
 package main
 
-
 import (
     "fmt"
     "os/exec"
@@ -12,6 +11,9 @@ import (
     "strings"
 )
 
+/**
+ * 获取某个用户的提交过的文件数量
+ */
 func filesOfUser(username string) int {
     git := fmt.Sprintf("git log --author=%s --pretty=%%H | while read commit_hash; do git show --oneline --name-only $commit_hash | tail -n+2; done | sort | uniq | wc -l", username)
     out, err := exec.Command("sh", "-c", git).Output()
@@ -19,27 +21,50 @@ func filesOfUser(username string) int {
         log.Fatal(err)
     }
 
+    println(username + " ...")
+
     count := fmt.Sprintf("%s", out)
     count = strings.TrimSpace(count)
-    i, e := strconv.Atoi(count)
-    if e != nil {
-        log.Fatal(e)
+    n, err := strconv.Atoi(count)
+    if err != nil {
+        log.Fatal(err)
     }
 
-    return i
+    return n
 }
 
+/**
+ * 找出所有VCS用户名
+ */
+func getUsers() []string {
+    git := "git log --pretty=format:%an | sort | uniq"
+    out, err := exec.Command("sh", "-c", git).Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    o := fmt.Sprintf("%s\n", out)
+    return strings.Split(o, "\n")
+}
+
+/**
+ * 总排行
+ */
 func topUsers() map[string] int {
     users := make(map[string] int)
-    usernames := []string{"gaopeng", "ouyangde"}
+    usernames := getUsers()
+    fmt.Println(usernames)
     for _, name := range usernames {
-        users[name] = filesOfUser(name)
+        if len(strings.TrimSpace(name)) > 0 && !strings.Contains(name, " ") {
+            users[name] = filesOfUser(name)
+        }
     }
 
     return users
 }
 
 func main() {
+    getUsers()
     result := topUsers()
     for k, v := range result {
         fmt.Printf("%15s\t%d\n", k, v)
