e7765e80209308442401b1d9c21f50c05355efe2
gaopeng <gaopeng@corp.kaixin001.com>
2012-09-26 15:13:07 +0800

    timeout on chan

---
+++ lang/go/timeout.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "time"
+)
+
+func main() {
+    var c chan bool = make(chan bool)
+    c1 := make(chan int)
+
+    go func() {
+        time.Sleep(1e9)
+        c <- true
+    }()
+
+    select {
+    case x := <- c1:
+        println("omg", x)
+    case <- c:
+        println("timeout")
+    }
+}
