744785d168df4791bf6cdb1feedeaa03d28c49a5
funky gao <funky.gao@gmail.com>
2012-10-12 21:45:59 +0800

    add line validator

--- lang/go/amf.go
+++ lang/go/amf.go
@@ -1,28 +1,40 @@
-// lzop -dcf /kx/dlog/121012/lz.121012-11* | grep AMF_SLOW | grep "100\.123" | grep "KP:PHP\.CDlog" | amf
+// lzop -dcf /kx/dlog/121012/lz.121012-11* | ./amf
 package main
 
 import (
-    "fmt"
     "bufio"
+    "fmt"
+    "io"
     "log"
     "os"
     "runtime"
-    "strings"
     "strconv"
+    "strings"
 )
 
+// a single line meta info
 type Request struct {
     http_method, uri, rid, class, method, args string
-    time int 
+    time int16
 }
 
-func (req *Request) parse(line string) {
-    var err error
+// parse a line into meta info
+// ret -> valid line?
+func (req *Request) parse(line string, validator func(string) bool) bool {
+    if !validator(line) {
+        return false
+    }
+
     parts := strings.Split(line, " ")
 
+    // uri related
     uriInfo := strings.Split(parts[5], "+")
+    if len(uriInfo) < 3 {
+        log.Fatal(parts[5])
+    }
     req.http_method, req.uri, req.rid = uriInfo[0], uriInfo[1], uriInfo[2]
 
+    // class call and args related
     callRaw := strings.Replace(parts[6], "{", "", -1) 
     callRaw = strings.Replace(callRaw, "}", "", -1) 
     callRaw = strings.Replace(callRaw, "\"", "", -1) 
@@ -30,29 +42,35 @@ func (req *Request) parse(line string) {
     callRaw = strings.Replace(callRaw, "]", "", -1) 
     callRaw = strings.Replace(callRaw, ",", ":", -1) 
     callInfo := strings.Split(callRaw, ":")
-    req.time, err = strconv.Atoi(callInfo[1])
+    time, err := strconv.Atoi(callInfo[1])
     if err != nil {
         log.Fatal(err)
     }
+    req.time = int16(time)
     req.class = callInfo[3]
     if len(callInfo) > 10 {
         req.method = callInfo[10]
     }
+
+    return true // valid
 }
 
+// better printable Request
 func (req *Request) String() string {
     return fmt.Sprintf("{http:%s uri:%s rid:%s class:%s method:%s time:%d args:%s}",
     req.http_method, req.uri, req.rid, req.class, req.method, req.time, req.args)
 }
 
+// read lines from stdin
 func readLines() {
     inputReader := bufio.NewReader(os.Stdin)
     lineCh := make(chan string, 10)
     for {
         line, err := inputReader.ReadString('\n')
         if err != nil {
-            // EOF
-            fmt.Println(err)
+            if err != io.EOF {
+                log.Fatal(err)
+            }
             break
         }
 
@@ -61,11 +79,27 @@ func readLines() {
     }
 }
 
+// line validator
+func lineValidator(line string) bool {
+    var items = []string{"AMF_SLOW", "100.123", "PHP.CDlog"}
+    for _, item := range items {
+        if !strings.Contains(line, item) {
+            return false
+        }
+    }
+
+    return true
+}
+
+// raw line handler
 func handleLine(lineCh chan string) {
     var line string = <- lineCh
 
     req := new(Request)
-    req.parse(line)
+    if !req.parse(line, lineValidator) {
+        return
+    }
+
     fmt.Printf("%5d%36s%20s %s\n", req.time, req.uri, req.class, req.method)
 }
 
@@ -74,3 +108,4 @@ func main() {
 
     readLines()
 }
+
