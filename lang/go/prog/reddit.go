9d9982a1c56173ccb964de9b9f5fd996a72106e1
gaopeng <gaopeng@corp.kaixin001.com>
2012-11-02 14:03:18 +0800

    reddit cli

---
+++ lang/go/prog/reddit.go
@@ -0,0 +1,40 @@
+// reddit CLI
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Item struct {
+	Title, Url string
+}
+
+type Response struct {
+	Data struct {
+		Children []struct {
+			Data Item
+		}
+	}
+}
+
+func main() {
+    fmt.Println(os.Args)
+    var query = "golang"
+    if len(os.Args) == 2 {
+        query = os.Args[1]
+    }
+    url := fmt.Sprintf("http://www.reddit.com/r/%s.json", query)
+	resp, e := http.Get(url)
+	if e != nil {
+		panic(e)
+	}
+
+	r := new(Response)
+	json.NewDecoder(resp.Body).Decode(r)
+	for _, d := range r.Data.Children {
+		fmt.Printf("%s\n\t%s\n\n", d.Data.Title, d.Data.Url)
+	}
+}
