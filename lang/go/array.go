0738abd9676d5cbb728a8eef6622c143916abe5b
funky gao <funky.gao@gmail.com>
2012-11-05 21:54:15 +0800

    upload image web server finished

---
+++ lang/go/array.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var x = [10]int{2:1, 8:5}
+    fmt.Println(x)
+}
--- lang/go/prog/web/upload.go
+++ lang/go/prog/web/upload.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+    "fmt"
     "io"
     "io/ioutil"
     "net/http"
     "text/template"
 )
 
-var uploadTemplate, e = template.ParseFiles("upload.html")
-
+var uploadTemplate, _ = template.ParseFiles("upload.html")
+var errorTemplate, _ = template.ParseFiles("error.html")
 
 func main() {
-    http.HandleFunc("/", upload)
+    if uploadTemplate == nil || errorTemplate == nil {
+        panic("template file not exist")
+    }
+
+    http.HandleFunc("/", errorHandler(upload))
+    http.HandleFunc("/view", errorHandler(view))
     http.ListenAndServe(":8090", nil)
+}
+
+func errorHandler(fn http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        defer func() {
+            if e := recover(); e != nil {
+                w.WriteHeader(500)
+                errorTemplate.Execute(w, e)
+            }
+        }()
+        fn(w, r)
+    }
+}
+
+func checkError(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("%#v\n", r)
+    w.Header().Set("Content-Type", "image")
+    http.ServeFile(w, r, r.FormValue("id"))
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
@@ -22,22 +52,15 @@ func upload(w http.ResponseWriter, r *http.Request) {
     }
 
     f, _, e := r.FormFile("image")
-    if e != nil {
-        http.Error(w, e.Error(), 500)
-    }
+    checkError(e)
     defer f.Close()
 
     t, e := ioutil.TempFile(".", "image-")
-    if e != nil {
-        http.Error(w, e.Error(), 500)
-        return
-    }
+    checkError(e)
     defer t.Close()
 
-    if _, e := io.Copy(t, f); e != nil {
-        http.Error(w, e.Error(), 500)
-        return
-    }
+    _, e = io.Copy(t, f)
+    checkError(e)
     http.Redirect(w, r, "/view?id=" + t.Name(), 302)
 }
 
