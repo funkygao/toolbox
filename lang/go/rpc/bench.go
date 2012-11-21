740088e199040dc10d4c3ff66a24b5639cc310a5
gaopeng <gaopeng@corp.kaixin001.com>
2012-11-21 11:46:38 +0800

    benchmark rpc

---
+++ lang/go/rpc/bench.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "testing"
+    "net/rpc"
+)
+
+type Args struct {
+    A, B int 
+}
+
+type Quotient struct {
+    Quo, Rem int 
+}
+
+func callRpc() {
+    client, e := rpc.DialHTTP("tcp", ":1234")
+    if e != nil {
+        panic(e)
+    }
+    defer client.Close()
+
+    args := Args{5, 2}
+    var reply Quotient
+
+    // sync call
+    e = client.Call("Math.Divide", args, &reply)
+    if e != nil {
+        panic(e)
+    }
+}
+
+
+
+func main() {
+    fmt.Printf("%s\n", testing.Benchmark(benchmarkRpcCallDivide).String())
+}
+
+func benchmarkRpcCallDivide(b *testing.B) {
+    for i:=0; i<b.N; i++ {
+        callRpc()
+    }
+}
