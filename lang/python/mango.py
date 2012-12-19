e33770868eac8989a9da91f4a70dd237a9bd6d02
gaopeng <gaopeng@corp.kaixin001.com>
2012-12-19 15:21:32 +0800

    man for golang

---
+++ lang/python/mango.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
+import re
+import sys
+
+r = re.compile(r'pkg (?P<pkg>\S+), func (?P<func>\S+).*')
+apifile = '/usr/local/go/api/go1.txt'
+
+
+def main(tag):
+    with open(apifile) as f:
+        for l in f:
+            m = r.match(l)
+            if not m:
+                continue
+            d = m.groupdict()
+            pkg, func = d['pkg'], d['func']
+            for t in tag:
+                if pkg.find(t)!=-1 or func.find(t)!=-1:
+                    print pkg, func
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print '''Usage:
+        %s keyword, [keyword]''' % sys.argv[0]
+        sys.exit(0)
+
+    main(sys.argv[1:])
