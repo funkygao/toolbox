5264db13a823e9f42e4ac090a384f3d085b9ef50
funkygao <funky.gao@gmail.com>
2013-09-13 09:48:44 +0800

    print header periodically

--- fun/ngtop/director.py
+++ fun/ngtop/director.py
@@ -7,12 +7,15 @@ import socket
 import sys
 import time
 
+HEAD_REPEAT_STEP = 30
 SELECT_TIMEOUT = 1
 BUFSIZE = 1024
 
-def print_head(domain, ips):
-    print '=' * 5, domain, '=' * 5
-    print ips, "\n"
+def print_head(domain = '', ips = []):
+    if domain:
+        print '=' * 5, domain, '=' * 5
+    if ips:
+        print ips, "\n"
     print '%-16s %-8s %-8s %-10s %-5s %-5s %-5s %-10s %-20s' % (
             'Nginx', 'Now', 'Conn', 'Conn/s', 'Read', 'Write', 'Wait', 'RPS', 'RPSbar')
     print '-'*16, '-------- -------- ---------- ----- ----- ----- ----------', '-' * 20
@@ -34,6 +37,7 @@ def main():
     print_head(domain, ips)
 
     nginx_status_procs = [Popen(['python', 'nginx_status.py', ip, str(ipcolor)], stdout = PIPE, bufsize = BUFSIZE) for ipcolor, ip in enumerate(ips)]
+    count = 0
     try:
         while nginx_status_procs:
             for proc in nginx_status_procs:
@@ -43,6 +47,9 @@ def main():
                 else:
                     if select.select([proc.stdout], [], [], SELECT_TIMEOUT)[0]:
                         handle_nginx_status(proc.stdout)
+                        count += 1
+                        if count % HEAD_REPEAT_STEP == 0:
+                            print_head()
             else:
                 time.sleep(1)
                 continue
