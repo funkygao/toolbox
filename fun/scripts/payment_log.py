587702c92d7cd82bc7ca87d04707e23560ba7370
funkygao <funky.gao@gmail.com>
2013-10-02 12:48:31 +0800

    payment log analyzer

---
+++ fun/scripts/payment_log.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''dlog payment_* analyzer'''
+
+import json
+import sys
+from collections import defaultdict
+
+total, levels, items = defaultdict(int), defaultdict(int), defaultdict(int)
+
+for line in sys.stdin:
+    locale, ts, data = line.strip().split(',', 2)
+    info = json.loads(data)
+    t, data = info['type'], info['data']
+    if t != 'OK':
+        print t
+        continue
+
+    total[data['currency']] += int(data['amount'])
+    levels[data['level']] += 1
+    items[data['item']] += 1
+
+def print_ordered_result(title, d):
+    print '*' * 10, title, '*'*10
+    r = sorted( ((v, k) for k, v in d.iteritems()), reverse=True)
+    for k, v in r.iteritems():
+        print v, k
+    print
+
+print_ordered_result('mongy', total)
+print_ordered_result('level', levels)
+print_ordered_result('type', items)
