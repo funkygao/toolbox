1709475b0e2ba2e7c4dc66df5e81f9bf4a80bad2
funkygao <funky.gao@gmail.com>
2013-10-02 13:09:27 +0800

    threshold

--- fun/scripts/payment_log.py
+++ fun/scripts/payment_log.py
@@ -3,12 +3,13 @@
 
 import json
 import sys
+import locale
 from collections import defaultdict
 
 total, levels, items = defaultdict(int), defaultdict(int), defaultdict(int)
 
 for line in sys.stdin:
-    locale, ts, data = line.strip().split(',', 2)
+    area, ts, data = line.strip().split(',', 2)
     info = json.loads(data)
     t, data = info['type'], info['data']
     if t != 'OK':
@@ -19,13 +20,17 @@ for line in sys.stdin:
     levels[data['level']] += 1
     items[data['item']] += 1
 
-def print_ordered_result(title, d):
+def print_ordered_result(title, d, threshold=None):
     print '*' * 10, title, '*'*10
     r = sorted( ((v, k) for k, v in d.iteritems()), reverse=True)
     for k, v in r:
-        print "%10s %s" % (v, "{:,.2f}".format(k))
+        if threshold is not None and k < threshold:
+            break
+        # print "%10s %s" % (v, "{:,d}".format(k)) # only works above python2.7
+        print "%10s %s" % (v, locale.format("%d", k, grouping=True))
     print
 
+locale.setlocale(locale.LC_ALL, '')
 print_ordered_result('mongy', total)
-print_ordered_result('level', levels)
+print_ordered_result('level', levels, 100)
 print_ordered_result('type', items)
