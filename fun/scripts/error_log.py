fe092fab5bbc152349c404c40177897586375689
funkygao <funky.gao@gmail.com>
2013-10-11 12:27:36 +0800

    normalize err msg

--- fun/scripts/error_log.py
+++ fun/scripts/error_log.py
@@ -3,10 +3,14 @@
 
 import json
 import sys
+import re
+
+digitRegex = re.compile(r'\d+')
 
 for line in sys.stdin:
     locale, ts, data = line.strip().split(',', 2)
     info = json.loads(data) # class, code, level, message, flash_version_client, batch_token
-    print info['message']
+    msg = info['message']
+    print digitRegex.sub('?', msg)
     #print info['class']
     #print info['code']
