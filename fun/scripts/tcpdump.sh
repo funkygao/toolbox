e8a35de5eff2a482fa90e9bc6f3c12634476a7da
funkygao <funky.gao@gmail.com>
2013-10-17 17:18:59 +0800

    tcpdump more tips

--- fun/scripts/tcpdump.sh
+++ fun/scripts/tcpdump.sh
@@ -5,6 +5,7 @@
 #tcpdump -n -vvxXs 1500 -i eth0 'port 11211 and tcp[13] & 8 = 8'
 #tcpdump -n -vvvAs 1500 -i eth0 'port 11211 and (tcp[13] & 8 = 8 or tcp[13] & 2 != 0 or tcp[13] & 16 != 0)'
 tcpdump -n -vvvAs 1500 -i eth0 'port 11211 and tcp[13] & 8 = 8'
+tcpdump -n -vvvAs 1500 -i eth0 '(dst port 11211 or dst port 11212 or dst port 11213)'
 
 # 统计一下各种mongodb查询的情况 
 #========================
@@ -12,7 +13,7 @@ tcpdump -vvvAs 1500 -n 'dst port 27017 and tcp[13] & 8 = 8' | grep --color royal
 cat /mnt/logs/mongo.log | cut -d'.' -f2 | sort | uniq -c | sort -nr 
 
 # find dup tcp handshake sync
-# the '-S' above which keeps sequence numbers intact
+# the '-S' above which keeps sequence numbers intact(absolute instead of relative sequence number)
 # this will be helpful to troubleshoot issues if some retransmits are found
 # will write the logs to a pcap file conn.pcap, which can be analyzed in wireshark
 #========================
