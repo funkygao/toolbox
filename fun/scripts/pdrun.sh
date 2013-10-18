7c223b1d16070c1b4bba04cd0f742f3e10718cb9
funkygao <funky.gao@gmail.com>
2013-10-18 10:44:57 +0800

    pdsh usage

---
+++ fun/scripts/pdrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+usage(){
+    echo "Usage: $0 (us|th|fr|de|ae|nl) (mongo|web|proxy|cache|log) [CMD]" >&2
+}
+
+hostlist=$(hosts | grep $1,$2 | cut -d',' -f3|awk '$1 !~ /^$/ {host = $1; if(FNR == 1){list[length(list)+1] = host}else{list[length(list)+1] = ","host}}END{ for(k=1;k<=length(list);k++){printf list[k]}}')
+
+if [ "$#" -eq 0 ];then
+    usage
+    exit
+fi
+
+echo $hostlist
+pdsh -b -R ssh -l root -u 10 -w $hosts $3
+
