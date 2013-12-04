8b15b1c7d2a6b5fb72f6230fbb058a651de1e4c7
funkygao <funky.gao@gmail.com>
2013-12-04 10:03:12 +0800

    regex named group sample

---
+++ lang/go/regex.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// embed regexp.Regexp in a new type so we can extend it
+type myRegexp struct {
+	*regexp.Regexp
+}
+
+// add a new method to our new regular expression type
+func (r *myRegexp) FindStringSubmatchMap(s string) map[string]string {
+	captures := make(map[string]string)
+
+	match := r.FindStringSubmatch(s)
+	if match == nil {
+		return captures
+	}
+
+	for i, name := range r.SubexpNames() {
+		// Ignore the whole regexp match and unnamed groups
+		if i == 0 || name == "" {
+			continue
+		}
+
+		captures[name] = match[i]
+
+	}
+	return captures
+}
+
+// an example regular expression
+var myExp = myRegexp{regexp.MustCompile(`(?P<first>\d+)\.(\d+).(?P<second>\d+)`)}
+
+func main() {
+	fmt.Printf("%#v", myExp.FindStringSubmatchMap("1234.5678.9"))
+}
