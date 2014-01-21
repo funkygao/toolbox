cbd8552ec474870cc99c4060cdf459826c8d381f
funkygao <funky.gao@gmail.com>
2014-01-22 00:03:46 +0800

    ping

---
+++ lang/go/ping.go
@@ -0,0 +1,69 @@
+/* Ping   
+*/
+package main   
+
+import (   
+    "fmt"
+    "net"
+    "os"
+)   
+
+func main() {   
+    if len(os.Args) != 2 {   
+        fmt.Println("Usage: ", os.Args[0], "host")   
+        os.Exit(1)   
+    }   
+    addr, err := net.ResolveIPAddr("ip", os.Args[1])   
+    if err != nil {   
+        fmt.Println("Resolution error", err.Error())   
+        os.Exit(1)   
+    }   
+    conn, err := net.DialIP("ip:icmp", nil, addr)   
+    if err != nil{   
+        fmt.Println("dial err")   
+    }   
+    checkError(err)   
+    var msg [8]byte   
+    msg[0] = 8  // echo   
+    msg[1] = 0  // code 0
+    msg[2] = 0  // checksum, fix later   
+    msg[3] = 0  // checksum, fix later   
+    msg[4] = 0  // identifier[0]   
+    msg[5] = 13 //identifier[1]   
+    msg[6] = 0  // sequence[0]   
+    msg[7] = 37 // sequence[1]   
+    len := 8
+    check := checkSum(msg[0:len])   
+    msg[2] = byte(check >> 8)   
+    msg[3] = byte(check & 255)   
+    _, err = conn.Write(msg[0:len])   
+    checkError(err)   
+    _, err = conn.Read(msg[0:])   
+    checkError(err)   
+    fmt.Println("Got Response")   
+    if msg[5] == 13 {   
+        fmt.Println("identifier matches")   
+    }   
+    if msg[7] == 37 {   
+        fmt.Println("Sequence matches")   
+    }   
+    os.Exit(0)   
+}   
+func checkSum(msg []byte) uint16 {   
+    fmt.Println(len(msg))   
+    sum := 0
+    // assume even for now   
+    for n := 1; n < len(msg)-1; n += 2 {   
+        sum += int(msg[n])*256 + int(msg[n+1])   
+    }   
+    sum = (sum >> 16) + (sum & 0xffff)   
+    sum += (sum >> 16)   
+    var answer uint16 = uint16(^sum)   
+    return answer   
+}   
+func checkError(err error) {   
+    if err != nil {   
+        fmt.Fprintln(os.Stderr, "Fatal error:", err.Error())   
+        os.Exit(1)   
+    }   
+}
