2cdae27dbb500027561d44fc9d8fee33d8b65d20
funkygao <funky.gao@gmail.com>
2014-02-26 09:49:39 +0800

    specify data size

--- fun/memcache/dos.go
+++ fun/memcache/dos.go
@@ -5,21 +5,25 @@ import (
 	"fmt"
 	"github.com/bradfitz/gomemcache/memcache"
 	"runtime"
+	"strings"
 	"sync/atomic"
 	"time"
 )
 
 var (
 	concurrent int64
+	dataSize   int
 	host       string
 	port       string
 	timeout    = time.Second * 10
+	interval   = time.Millisecond * 20
 )
 
 func main() {
 	var c int
 	flag.StringVar(&host, "h", "54.184.78.104", "host")
 	flag.StringVar(&port, "p", "11211", "port")
+	flag.IntVar(&dataSize, "l", 1024, "cache value size in bytes")
 	flag.IntVar(&c, "c", 2000, "concurrent conn")
 	flag.Parse()
 
@@ -29,10 +33,14 @@ func main() {
 
 	mc := memcache.New(mcServer)
 	mc.Timeout = timeout
-	if err := mc.Set(&memcache.Item{Key: "foo", Value: []byte("my valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy valuemy value")}); err != nil {
+
+	if err := mc.Set(&memcache.Item{
+		Key:   "foo",
+		Value: []byte(strings.Repeat("X", dataSize))}); err != nil {
 		panic(err)
 	}
 	for i := 0; i < c; i++ {
+		//time.Sleep(interval)
 		go func() {
 			for {
 				mc := memcache.New(mcServer)
@@ -41,7 +49,7 @@ func main() {
 				for {
 					_, err := mc.Get("foo")
 					if err != nil {
-						fmt.Printf("%s ", err)
+						fmt.Printf("%s\n", err)
 						break
 					}
 				}
