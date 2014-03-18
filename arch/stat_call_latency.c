b603e12a1ba643b47474dc712a20c2c41d790297
funkygao <funky.gao@gmail.com>
2014-03-18 15:52:49 +0800

    more detailed report

--- arch/stat_call_latency.c
+++ arch/stat_call_latency.c
@@ -1,19 +1,36 @@
 #include <sys/stat.h>
-#include<stdio.h>   
-#include<time.h>  
+#include <stdio.h>   
+#include <time.h>  
 
 #define N 1000000
 
-int main() 
-{
+float measure_stat_latency(char *filename) {
     clock_t t1, t2;
     int i;
     struct stat buf;
+
     t1 = clock();
     for (i=0; i<N; i++) {
-        stat("stat_call_latency.c", &buf);
+        stat(filename, &buf);
     }
     t2 = clock();
     float deltaMs= (((float)t2 - (float)t1) / 1000000.0F ) * 1000;   
-    printf("%.5fms\n", deltaMs);   
+    printf("%25s: %.5fms\n", filename, deltaMs);   
+    return deltaMs;
+}
+
+int main() {
+    struct stat buf;
+    printf("sizeof stat: %lu\n", sizeof(buf));
+
+    float t1, t2;
+    t1 = measure_stat_latency("stat_call_latency.c"); /* this file itself */
+    t2 = measure_stat_latency("non-exist-file"); 
+    printf("N=%d\n%50s: %.5fms\n%50s: %.5fms\n", 
+            N, 
+            "copy stat from kernal to user space time",
+            t1-t2,
+            "vfs_stat:fget+vfs_getattr+fput",
+            t2
+          );
 }
