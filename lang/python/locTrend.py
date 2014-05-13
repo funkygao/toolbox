210341f38935c49f6b806051f3cb8b97886d32f9
funkygao <funky.gao@gmail.com>
2014-05-13 21:12:33 +0800

    improvement for readability

--- lang/python/locTrend.py
+++ lang/python/locTrend.py
@@ -6,11 +6,12 @@ import subprocess
 # show current dir git log stat num
 cmd = 'git log --shortstat --since "%d weeks ago" --until "%d week ago" ./| grep "files\? changed" | awk \'{files+=$1; inserted+=$4; deleted+=$6} END {print "files", files, "inserted:", inserted, "deleted:", deleted}\''
 
-for i in range(1, 30):
-    log = subprocess.Popen(cmd % (i+1, i), shell=True, stdout=subprocess.PIPE).stdout.read()
-    parts = log.split()
+YEAR = 3
+for week in reversed(range(1, 52*YEAR)): # weekly contributions for 3 years in reverse order
+    parts = subprocess.Popen(cmd % (week+1, week), shell=True, stdout=subprocess.PIPE).stdout.read().split()
     if len(parts) != 6:
         continue
+
     files_changed, lines_added, lines_deleted = parts[1], parts[3], parts[5]
-    print lines_added, lines_deleted
+    print week, lines_added, lines_deleted
 
