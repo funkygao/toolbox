36f8e51865da7376cc737a32c100a15f903473d4
funkygao <funky.gao@gmail.com>
2015-06-24 17:04:20 +0800

    forgot to close db conn

--- lang/go/db/mysql.go
+++ lang/go/db/mysql.go
@@ -16,39 +16,53 @@ func dieIfErr(err error) {
 	}
 }
 
+var idgen chan int
+
 func main() {
 	var wg sync.WaitGroup
 	t0 := time.Now()
+	idgen = make(chan int, 1000)
+	go func() {
+		id := 0
+		for {
+			id++
+			idgen <- id
+		}
+	}()
+	const C = 500
+	const Loop = 10000
+
 	if os.Args[1] == "insert" {
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < C; i++ {
 			wg.Add(1)
 			go func(seq int) {
-				insert(seq, 10000)
+				insert(seq, Loop)
 				wg.Done()
 			}(i)
 		}
 
 	} else {
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < C; i++ {
 			wg.Add(1)
 			go func(seq int) {
-				update(seq, 10000)
+				update(seq, Loop)
 				wg.Done()
 			}(i)
 		}
 	}
 
 	wg.Wait()
-	fmt.Println(time.Since(t0))
+	fmt.Println(time.Since(t0), time.Since(t0).Seconds())
 
 }
 
 func insert(seq int, loops int) {
 	db, err := sql.Open("mysql", "dbtest:dbtest@tcp(10.77.145.36:3306)/dbtest?charset=utf8")
 	dieIfErr(err)
+	defer db.Close()
 
 	for i := 0; i < loops; i++ {
-		uid := seq*100000 + i
+		uid := <-idgen
 		query := "INSERT INTO user(uid, name, age) values(?,?,?)"
 		db.Exec(query, uid, "foo", 20)
 	}
@@ -58,11 +72,12 @@ func insert(seq int, loops int) {
 func update(seq int, loops int) {
 	db, err := sql.Open("mysql", "dbtest:dbtest@tcp(10.77.145.36:3306)/dbtest?charset=utf8")
 	dieIfErr(err)
+	defer db.Close()
 
 	for i := 0; i < loops; i++ {
-		uid := seq*100000 + i
+		uid := <-idgen
 		query := "UPDATE user SET name=? WHERE uid=?"
-		db.Exec(query, "bar", uid, "foo")
+		db.Exec(query, "bar", uid)
 	}
 
 }
