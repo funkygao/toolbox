f79c01653c8139c53951fc9d5e01d1a33e80f184
funkygao <funky.gao@gmail.com>
2015-08-07 16:46:12 +0800

    php two phase commit example

---
+++ lang/php/2pc.php
@@ -0,0 +1,44 @@
+<?php
+
+// innodb_support_xa=ON
+// tx_isolation=SERIALIZABLE
+
+$foo = new mysqli("10.128.51.121","public","public","db1") or die("$foo：连接失败");
+$map = new mysqli("10.128.51.122","public","public","db2") or die("$map：连接失败");
+
+$grid = uniqid(""); // xid
+// 1pc start
+$map->query("XA START '$grid'");//准备事务1
+$foo->query("XA START '$grid'");//准备事务2
+
+
+try {
+    $return = $map->query("UPDATE test_transation2 SET name='foo' WHERE id=2") ; //第一个分支事务准备做的事情，通常他们会记录进日志
+    if($return == false) {
+        throw new Exception("map");
+    }
+
+    $return = $foo->query("UPDATE test_transation1 SET name='bar' WHERE id=1"); //第二个分支事务准备做的事情，通常他们会记录进日志
+    if($return == false) {
+        throw new Exception("foo");
+    }
+
+    // 1pc end
+    $map->query("XA END '$grid'");
+    $foo->query("XA END '$grid'");
+
+    // 2pc prepare
+    $map->query("XA PREPARE '$grid'");
+    $foo->query("XA PREPARE '$grid'");
+
+    // 2pc commit
+    $foo->query("XA COMMIT '$grid'");
+    $map->query("XA COMMIT '$grid'");
+} catch (Exception $e) {
+    $foo->query("XA ROLLBACK '$grid'");
+    $map->query("XA ROLLBACK '$grid'");
+    print $e->getMessage();
+}
+
+$map->close();
+$foo->close();
